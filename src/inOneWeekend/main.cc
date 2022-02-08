afe509cfdb78eb0de61a20ad215407dd7b806afb
lavijiang <245348518@qq.com>
2022-02-08 21:26:40 +0800

    chapter6

--- src/inOneWeekend/main.cc
+++ src/inOneWeekend/main.cc
@@ -2,21 +2,27 @@
 #include "ray.h"
 
 using namespace std;
-bool hit_sphere(const vec3& center,float radius,const ray& r){
+float hit_sphere(const vec3& center,float radius,const ray& r){
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(),r.direction());
     float b = 2.0 * dot(oc,r.direction());
     float c = dot(oc,oc)-radius*radius;
     float discri = b*b-4*a*c;
-    return discri>0;
+    if(discri<0){
+        return -1;
+    }else{
+        return (-b-sqrt(discri))/(2.0*a);
+    }
 }
 
 vec3 colors(const ray& r){
-    if (hit_sphere(vec3(0,0,-1),0.5,r)){
-        return vec3(1,0,0);
+    float t = hit_sphere(vec3(0,0,-1),0.5,r);
+    if(t>0){
+        vec3 N = unit_vector(r.at(t)-vec3(0,0,-1));
+        return 0.5*vec3(N.x()+1,N.y()+1,N.z()+1);
     }
     vec3 unit_dir = unit_vector(r.direction());
-    float t = 0.5*(unit_dir.y()+1.0);
+    t = 0.5*(unit_dir.y()+1.0);
     return (1.0-t)*vec3(1.0,1.0,1.0)+t*vec3(0.5,0.7,1);
 }
 
